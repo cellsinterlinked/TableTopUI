4e5613cdada99cb59e20ca4879f0c40fb9bc8c73
Scott <scott_hoffmann1879@outlook.com>
2022-01-26 21:54:27 -0500

    oops

--- src/Pages/Play.js
+++ src/Pages/Play.js
@@ -108,8 +108,8 @@ const Play = ({ location }) => {
       : []
   );
 
-  // const ENDPOINT = 'https://table-top-sever.herokuapp.com/'
-  const ENDPOINT = 'http://localhost:5000';
+  const ENDPOINT = 'https://table-top-sever.herokuapp.com/'
+  // const ENDPOINT = 'http://localhost:5000';
 
   useEffect(() => {
     const { name, room, role } = queryString.parse(location.search);
